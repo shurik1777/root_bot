dc481aec4d8300ecdc6f5d950f481793134978ee
Wosmerik Alexandr <shurik1777@gmail.com>
2024-07-13 17:04:22 +0300

    add 5 handlers

--- app/handlers.py
+++ app/handlers.py
@@ -1,6 +1,7 @@
 from aiogram import Router
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
+from aiogram.enums.dice_emoji import DiceEmoji
 
 router = Router()
 
@@ -8,3 +9,31 @@ router = Router()
 @router.message(CommandStart())
 async def cmd_start(message: Message):
     await message.answer(f'Hello {message.from_user.first_name}')
+
+
+# Хэндлер на команду /test1 подключаем через декоратор
+@router.message(Command("test1"))
+async def cmd_test1(message: Message):
+    await message.reply("Test 1")
+
+
+# Хэндлер на команду /test2 подключаем через точечную аннатацию
+async def cmd_test2(message: Message):
+    await message.reply("Test 2")
+
+router.message.register(cmd_test2, Command("test2"))
+
+
+@router.message(Command("answer"))
+async def cmd_answer(message: Message):
+    await message.answer("Это простой ответ")
+
+
+@router.message(Command("reply"))
+async def cmd_reply(message: Message):
+    await message.reply('Это ответ с "ответом"')
+
+
+@router.message(Command("dice"))
+async def cmd_dice(message: Message):
+    await message.answer_dice(emoji="🎲")
